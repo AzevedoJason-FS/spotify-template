1477ea86297a2ff291116fafcdd8695852704c9e
Jason Azevedo <JCAzevedo@student.fullsail.edu>
2022-09-16 00:00:56 -0400

    comment out some code for later

--- spotify-app/src/pages/Search.js
+++ spotify-app/src/pages/Search.js
@@ -7,20 +7,22 @@ const Home = () => {
 
   React.useEffect(() => {
 
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken = urlParams.get('access_token');
-    const refreshToken = urlParams.get('refresh_token');
+    // const queryString = window.location.search;
+    // const urlParams = new URLSearchParams(queryString);
+    // const accessToken = urlParams.get('access_token');
+    // const refreshToken = urlParams.get('refresh_token');
 
-    console.log('access_token:'+ accessToken);
-    console.log('refresh_token:'+ refreshToken);
+    // console.log('access_token:'+ accessToken);
+    // console.log('refresh_token:'+ refreshToken);
 
-    if (refreshToken) {
-      fetch(`/refresh_token?refresh_token=${refreshToken}`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.error(err));
-    }
+    // if (refreshToken) {
+    //   fetch(`/refresh_token?refresh_token=${refreshToken}`)
+    //     .then(res => res.json())
+    //     .then(data => console.log(data))
+    //     .catch(err => console.error(err));
+    // }
+
+
 
 }, []);
 
